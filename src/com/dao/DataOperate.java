de248b286ab60dea54d0eed1b08cd5dd8e7b505c
JohnJayLiu <1414255897@qq.com>
2018-07-03 18:51:18 +0800

    大体结构

---
+++ src/com/dao/DataOperate.java
@@ -0,0 +1,5 @@
+package com.dao;
+//在这里定义数据库操作的接口
+public class DataOperate {
+
+}
---
+++ src/com/models/Student.java
@@ -0,0 +1,43 @@
+package com.models;
+
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
+public class Student {
+	private String studentName;
+	private String studentID;
+	private String password;
+	private String address;
+	private String phone;
+	
+	public void setStudentName(String studentName){
+		this.studentName=studentName;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentID(String studentID) {
+		this.studentID=studentID;
+	}
+	public String getStudentID() {
+		return studentID;
+	}
+	public void setPassword(String password) {
+		this.password=password;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setAddress(String address) {
+		this.address=address;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setPhone (String phone ) {
+		this.phone=phone;
+		
+	}
+	public String getPhone () {
+		return phone;
+	}
+}
