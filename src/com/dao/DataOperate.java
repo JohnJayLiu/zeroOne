c7d65550628449926a596ded70bb08cd0fbdafe8
JohnJayLiu <1414255897@qq.com>
2018-07-04 15:08:58 +0800

    学生数据操作

--- src/com/dao/DataOperate.java
+++ src/com/dao/DataOperate.java
@@ -1,5 +1,9 @@
 package com.dao;
+
+import com.models.*;
 //在这里定义数据库操作的接口
 public interface DataOperate {
-
+	public Student getStudentByID(String StudentID);
+	public void insertStudent(Student student);
+	public void updateStudent(Student student);
 }
--- src/com/servlet/LoginServlet.java
+++ src/com/servlet/LoginServlet.java
@@ -1,17 +1,33 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.dao.DataOperate;
+import com.models.Student;
+
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
+	static SqlSession session;
+	
+	public static void  setSession(SqlSession session1) {
+		session=session1;
+	}
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -27,7 +43,21 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String studentID=request.getParameter("username");
+		String password=request.getParameter("password");
+		List<Student>students;
+		students=session.selectList("com.dao.DataOperate", studentID);
+		if (students.size()==0) { }
+		else if (students.get(0).getPassword().equals(password)) {
+				
+			}
+		else {
+				
+			}
+		
+		
 	}
 
 	/**
