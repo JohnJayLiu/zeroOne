1dba5fb26e9c7cea2f567d200d0ad3566b3ee211
JohnJayLiu <1414255897@qq.com>
2018-07-09 11:54:19 +0800

    数据库

--- src/com/dao/DataOperate.java
+++ src/com/dao/DataOperate.java
@@ -11,4 +11,18 @@ public interface DataOperate {
     public void insertShopStock (ShopStock shopStock);
     public void updateShopStock(ShopStock shopStock);
     public void  daleteShopStock(String shopID);
+    
+    public Shop getShopID(String shopID);
+    public void insertShop (Shop shop);
+    public void updateShop (Shop shop);
+    public void  deleteShop(String shopID);
+    
+    public ShoppingCar getgoodsID(String goodsID);
+    public void insertShoppingCar(ShoppingCar shoppingCar);
+    public void updateShoppingCar(ShoppingCar shoppingCar);
+    public void deleteShoppingCar(String goodsID);
+    
+    public ShoppingCar coonQueryList(String goodsID);
+
+    public ShopStock coonSS(String shopID);
 }
--- src/com/models/ShoppingCar.java
+++ src/com/models/ShoppingCar.java
@@ -1,5 +1,40 @@
 package com.models;
 
-public class ShoppingCar {
+import java.util.List;
 
+public class ShoppingCar{
+    private String goodsID;
+    private String studentID;
+    private int count;
+    private ShopStock ShopStock;
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getGoodsID() {
+        return goodsID;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setGoodsID(String goodsID) {
+        this.goodsID = goodsID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setShopStock(ShopStock ShopStock) {
+        this.ShopStock = ShopStock;
+    }
+    public ShopStock getShopStock() {
+        return ShopStock;
+    }
 }
